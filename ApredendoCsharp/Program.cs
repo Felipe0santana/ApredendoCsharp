0f939d0b59cb80115bfdc34d22fb7e59f436a240
Felipe <Felipe@DESKTOP-DQL2IAU>
2022-04-14 09:13:34 -0300

    Adicionar arquivos de projeto.

---
+++ ApredendoCsharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApredendoCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola, Gatas");
+            Console.WriteLine("A aplicação terminou...!");
+            Console.ReadLine();
+        }
+    }
+}
